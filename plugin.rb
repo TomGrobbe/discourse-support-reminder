f54776bdd350042286336fb649bd5adc149f88da
Tom Grobbe <31419184+TomGrobbe@users.noreply.github.com>
2018-08-24 16:47:04 +0200

    Improved default reminder message.

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-support-reminder
 # about: Notifies the user when creating a support topic that support is provided by the community.
-# version: 1.0
+# version: 1.1
 # authors: Tom Grobbe
 # url: https://github.com/TomGrobbe/discourse-support-reminder
 
@@ -42,7 +42,7 @@ after_initialize do
             if target_categories and target_categories != "" and target_categories != "none"
                 reminder_message = SiteSetting.discourse_support_reminder_message
                 if !reminder_message or reminder_message == ""
-                    reminder_message = "Hi there, this is a small reminder because it has been a while since you have created a topic in this category.<br><br>Please note that support on these forums are provided by the FiveM community. Because of this, there is a small chance that your topic will not receive any replies to help you out. Sometimes the exact cause of your issue(s) are unknown, and therefor the community or FiveM staff is unable to assist you. To avoid unnecessary/duplicate topics, please browse the forums before creating a topic.<br><br>To improve your chances of a reply, please provide as much information as possible about the issue(s) you are having. Also, whenever possible, use the support template given to you as soon as you create a topic.<br><br>Thanks for helping us keeping these forums tidy! :mascot:"
+                    reminder_message = "Hello, this is a friendly reminder because this is your first time creating a topic (or it has been a while since your last topic) in this category.<br><br>Please note that most of the support is provided by the FiveM community on a voluntary basis. We ask you to be patient; there is no guarantee we have a solution to your problem(s). To avoid unnecessary/duplicate topics, please browse the forums before creating a topic.<br><br>To improve your chances of your issue(s) being solved, please provide as much information as possible about the issue(s) you are having. Also —whenever possible— please use the template given to you when creating a topic.<br><br>Thanks for keeping these forums tidy!<br>:mascot:"
                 end
                 target_categories.each do |target_category|
                     ignore_staff = SiteSetting.discourse_support_reminder_excempt_staff
